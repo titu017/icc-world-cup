dbe766095b0c87db2f17a32f828438efa9535208
Titu <titu017@gmail.com>
2019-04-18 15:56:27 +0600

    player update to branch

--- app/src/main/java/org/ajobdesh/wc/Pojo/SquadCollection.java
+++ app/src/main/java/org/ajobdesh/wc/Pojo/SquadCollection.java
@@ -17,83 +17,83 @@ public class SquadCollection {
         squadPlayer = new Squad();
 
         squadPlayer.setOne_serialTv("1");
-        squadPlayer.setTwo_TeamImageIv(R.drawable.bangladesh);
-        squadPlayer.setThree_teamNameTv("Bangladesh");
+        squadPlayer.setTwo_TeamImageIv(R.drawable.england);
+        squadPlayer.setThree_teamNameTv("England");
 
-        squadPlayer.setPlayer1Serial("1");
-        squadPlayer.setPlayer1firstName("Mashrafee ");
-        squadPlayer.setPlayer1lastName("Mortaza");
-        squadPlayer.setPlayer1Position("Bowler");
+        squadPlayer.setPlayer1Serial("1.");
+        squadPlayer.setPlayer1firstName("JOS ");
+        squadPlayer.setPlayer1lastName("BUTTLER");
+        squadPlayer.setPlayer1Position("WC-BATTER");
 
-        squadPlayer.setPlayer2Serial("2");
-        squadPlayer.setPlayer2firstName("Sakib ");
-        squadPlayer.setPlayer2lastName("Hasan");
-        squadPlayer.setPlayer2Position("All");
+        squadPlayer.setPlayer2Serial("2.");
+        squadPlayer.setPlayer2firstName("EOIN ");
+        squadPlayer.setPlayer2lastName("MORGAN (C)");
+        squadPlayer.setPlayer2Position("BATTER");
 
-        squadPlayer.setPlayer3Serial("3");
-        squadPlayer.setPlayer3firstName("Mashrafee ");
-        squadPlayer.setPlayer3lastName("Mortaza");
-        squadPlayer.setPlayer3Position("Bowler");
+        squadPlayer.setPlayer3Serial("3.");
+        squadPlayer.setPlayer3firstName("JOE ");
+        squadPlayer.setPlayer3lastName("DENLY");
+        squadPlayer.setPlayer3Position("BATTER");
 
-        squadPlayer.setPlayer4Serial("4 ");
-        squadPlayer.setPlayer4firstName("Mashrafee ");
-        squadPlayer.setPlayer4lastName("Mortaza");
-        squadPlayer.setPlayer4Position("Bowler");
+        squadPlayer.setPlayer4Serial("4.");
+        squadPlayer.setPlayer4firstName("ALEX ");
+        squadPlayer.setPlayer4lastName("HALES");
+        squadPlayer.setPlayer4Position("BATTER");
 
-        squadPlayer.setPlayer5Serial("5");
-        squadPlayer.setPlayer5firstName("Mashrafee");
-        squadPlayer.setPlayer5lastName("Mortaza");
-        squadPlayer.setPlayer5Position("Bowler");
+        squadPlayer.setPlayer5Serial("5.");
+        squadPlayer.setPlayer5firstName("JOE ");
+        squadPlayer.setPlayer5lastName("ROOT");
+        squadPlayer.setPlayer5Position("BATTER");
 
-        squadPlayer.setPlayer6Serial("6");
-        squadPlayer.setPlayer6firstName("Mashrafee");
-        squadPlayer.setPlayer6lastName("Mortaza");
-        squadPlayer.setPlayer6Position("Bowler");
+        squadPlayer.setPlayer6Serial("6.");
+        squadPlayer.setPlayer6firstName("JASON ");
+        squadPlayer.setPlayer6lastName("ROY");
+        squadPlayer.setPlayer6Position("BATTER");
 
-        squadPlayer.setPlayer7Serial("7");
-        squadPlayer.setPlayer7firstName("Mashrafee");
-        squadPlayer.setPlayer7lastName("Mortaza");
-        squadPlayer.setPlayer7Position("Bowler");
+        squadPlayer.setPlayer7Serial("7.");
+        squadPlayer.setPlayer7firstName("JONNY ");
+        squadPlayer.setPlayer7lastName("BAIRSTOW");
+        squadPlayer.setPlayer7Position("WC-BATTER");
 
-        squadPlayer.setPlayer8Serial("8");
-        squadPlayer.setPlayer8firstName("Mashrafee");
-        squadPlayer.setPlayer8lastName("Mortaza");
-        squadPlayer.setPlayer8Position("Bowler");
+        squadPlayer.setPlayer8Serial("8.");
+        squadPlayer.setPlayer8firstName("MOEEN ");
+        squadPlayer.setPlayer8lastName("ALI");
+        squadPlayer.setPlayer8Position("ALL-ROUNDER");
 
-        squadPlayer.setPlayer9Serial("9");
-        squadPlayer.setPlayer9firstName("Mashrafee");
-        squadPlayer.setPlayer9lastName("Mortaza");
-        squadPlayer.setPlayer9Position("Bowler");
+        squadPlayer.setPlayer9Serial("9.");
+        squadPlayer.setPlayer9firstName("BEN ");
+        squadPlayer.setPlayer9lastName("STOKES");
+        squadPlayer.setPlayer9Position("ALL-ROUNDER");
 
-        squadPlayer.setPlayer10Serial("10");
-        squadPlayer.setPlayer10firstName("Mashrafee");
-        squadPlayer.setPlayer10lastName("Mortaza");
-        squadPlayer.setPlayer10Position("Bowler");
+        squadPlayer.setPlayer10Serial("10.");
+        squadPlayer.setPlayer10firstName("LIAM ");
+        squadPlayer.setPlayer10lastName("PLUNKETT");
+        squadPlayer.setPlayer10Position("BOWLER");
 
-        squadPlayer.setPlayer11Serial("11");
-        squadPlayer.setPlayer11firstName("Mashrafee");
-        squadPlayer.setPlayer11lastName("Mortaza");
-        squadPlayer.setPlayer11Position("Bowler");
+        squadPlayer.setPlayer11Serial("11.");
+        squadPlayer.setPlayer11firstName("ADIL ");
+        squadPlayer.setPlayer11lastName("RASHID");
+        squadPlayer.setPlayer11Position("BOWLER");
 
-        squadPlayer.setPlayer12Serial("12");
-        squadPlayer.setPlayer12firstName("Mashrafee");
-        squadPlayer.setPlayer12lastName("Mortaza");
-        squadPlayer.setPlayer12Position("Bowler");
+        squadPlayer.setPlayer12Serial("12.");
+        squadPlayer.setPlayer12firstName("DAVID ");
+        squadPlayer.setPlayer12lastName("WILLEY");
+        squadPlayer.setPlayer12Position("BOWLER");
 
-        squadPlayer.setPlayer13Serial("13");
-        squadPlayer.setPlayer13firstName("Mashrafee");
-        squadPlayer.setPlayer13lastName("Mortaza");
-        squadPlayer.setPlayer13Position("Bowler");
+        squadPlayer.setPlayer13Serial("13.");
+        squadPlayer.setPlayer13firstName("CHRIS ");
+        squadPlayer.setPlayer13lastName("WOAKES");
+        squadPlayer.setPlayer13Position("BOWLER");
 
-        squadPlayer.setPlayer14Serial("14");
-        squadPlayer.setPlayer14firstName("Mashrafee");
-        squadPlayer.setPlayer14lastName("Mortaza");
-        squadPlayer.setPlayer14Position("Bowler");
+        squadPlayer.setPlayer14Serial("14.");
+        squadPlayer.setPlayer14firstName("TOM ");
+        squadPlayer.setPlayer14lastName("CURRAN");
+        squadPlayer.setPlayer14Position("BOWLER");
 
-        squadPlayer.setPlayer15Serial("15");
-        squadPlayer.setPlayer15firstName("Mashrafee");
-        squadPlayer.setPlayer15lastName("Mortaza");
-        squadPlayer.setPlayer15Position("Bowler");
+        squadPlayer.setPlayer15Serial("15.");
+        squadPlayer.setPlayer15firstName("MARK ");
+        squadPlayer.setPlayer15lastName("WOOD");
+        squadPlayer.setPlayer15Position("BOWLER");
         squadPlayers.add(squadPlayer);
 
 //        player 1 ends here
